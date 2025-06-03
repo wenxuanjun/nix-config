64b1bd9d3f2d5bea67c79f1385adeb71ae507bca
wenxuanjun <wenxuanjun@yahoo.com>
2025-06-03 13:24:01 +0800

    Add localization config

--- home/desktop/niri.nix
+++ home/desktop/niri.nix
@@ -1,60 +1,74 @@
 { config, lib, pkgs, ... }:
 
+let
+  workspaceBinds = builtins.listToAttrs (
+    builtins.concatMap (i: [
+      {
+        name = "Mod+${toString i}";
+        value = { action.focus-workspace = i; };
+      }
+      {
+        name = "Mod+Shift+${toString i}";
+        value = { action.move-column-to-workspace = i; };
+      }
+    ]) (builtins.genList (i: i + 1) 9)
+  );
+
+  appBinds = {
+    "Mod+Return".action.spawn = "fuzzel";
+    "Mod+BackSlash".action.spawn = "gnome-terminal";
+    "Mod+BackSpace".action.spawn = "thunar";
+  };
+
+  windowBinds = {
+    "Mod+Q".action.close-window = [];
+    "Mod+Left".action.focus-column-left = [];
+    "Mod+Down".action.focus-window-down = [];
+    "Mod+Up".action.focus-window-up = [];
+    "Mod+Right".action.focus-column-right = [];
+    "Mod+Shift+Left".action.move-column-left = [];
+    "Mod+Shift+Down".action.move-window-down = [];
+    "Mod+Shift+Up".action.move-window-up = [];
+    "Mod+Shift+Right".action.move-column-right = [];
+  };
+
+  workspaceNavBinds = {
+    "Mod+WheelScrollDown".action.focus-workspace-down = [];
+    "Mod+WheelScrollUp".action.focus-workspace-up = [];
+    "Mod+Shift+WheelScrollDown".action.move-column-to-workspace-down = [];
+    "Mod+Shift+WheelScrollUp".action.move-column-to-workspace-up = [];
+  };
+
+  sizeBinds = {
+    "Mod+F".action.maximize-column = [];
+    "Mod+Shift+F".action.fullscreen-window = [];
+    "Mod+C".action.center-column = [];
+    "Mod+Minus".action.set-column-width = "-10%";
+    "Mod+Equal".action.set-column-width = "+10%";
+    "Mod+Shift+Minus".action.set-window-height = "-20%";
+    "Mod+Shift+Equal".action.set-window-height = "+20%";
+  };
+
+  miscBinds = {
+    "Mod+P".action.power-off-monitors = [];
+    "Mod+V".action.toggle-window-floating = [];
+    "Print".action.screenshot = [];
+    "Ctrl+Print".action.screenshot-screen = [];
+    "Alt+Print".action.screenshot-window = [];
+  };
+in
 {
   programs.niri = {
     settings = {
-      binds = {
-        "Mod+Return".action.spawn = "fuzzel";
-        "Mod+BackSlash".action.spawn = "gnome-terminal";
-        "Mod+BackSpace".action.spawn = "thunar";
+      binds = lib.foldl lib.recursiveUpdate {} [
+        appBinds
+        windowBinds
+        workspaceNavBinds
+        sizeBinds
+        miscBinds
+        workspaceBinds
+      ];
 
-        "Mod+Q".action.close-window = [];
-        "Mod+Left".action.focus-column-left = [];
-        "Mod+Down".action.focus-window-down = [];
-        "Mod+Up".action.focus-window-up = [];
-        "Mod+Right".action.focus-column-right = [];
-        "Mod+Shift+Left".action.move-column-left = [];
-        "Mod+Shift+Down".action.move-window-down = [];
-        "Mod+Shift+Up".action.move-window-up = [];
-        "Mod+Shift+Right".action.move-column-right = [];
-        "Mod+WheelScrollDown".action.focus-workspace-down = [];
-        "Mod+WheelScrollUp".action.focus-workspace-up = [];
-        "Mod+Shift+WheelScrollDown".action.move-column-to-workspace-down = [];
-        "Mod+Shift+WheelScrollUp".action.move-column-to-workspace-up = [];
-
-        "Mod+1".action.focus-workspace = 1;
-        "Mod+2".action.focus-workspace = 2;
-        "Mod+3".action.focus-workspace = 3;
-        "Mod+4".action.focus-workspace = 4;
-        "Mod+5".action.focus-workspace = 5;
-        "Mod+6".action.focus-workspace = 6;
-        "Mod+7".action.focus-workspace = 7;
-        "Mod+8".action.focus-workspace = 8;
-        "Mod+9".action.focus-workspace = 9;
-        "Mod+Shift+1".action.move-column-to-workspace = 1;
-        "Mod+Shift+2".action.move-column-to-workspace = 2;
-        "Mod+Shift+3".action.move-column-to-workspace = 3;
-        "Mod+Shift+4".action.move-column-to-workspace = 4;
-        "Mod+Shift+5".action.move-column-to-workspace = 5;
-        "Mod+Shift+6".action.move-column-to-workspace = 6;
-        "Mod+Shift+7".action.move-column-to-workspace = 7;
-        "Mod+Shift+8".action.move-column-to-workspace = 8;
-        "Mod+Shift+9".action.move-column-to-workspace = 9;
-
-        "Mod+F".action.maximize-column = [];
-        "Mod+Shift+F".action.fullscreen-window = [];
-        "Mod+C".action.center-column = [];
-        "Mod+Minus".action.set-column-width = "-10%";
-        "Mod+Equal".action.set-column-width = "+10%";
-        "Mod+Shift+Minus".action.set-window-height = "-20%";
-        "Mod+Shift+Equal".action.set-window-height = "+20%";
-
-        "Mod+P".action.power-off-monitors = [];
-        "Mod+V".action.toggle-window-floating = [];
-        "Print".action.screenshot = [];
-        "Ctrl+Print".action.screenshot-screen = [];
-        "Alt+Print".action.screenshot-window = [];
-      };
       outputs = {
         "STD Computer Inc iPlaoe 123456789000" = {
           mode = {
@@ -64,9 +78,10 @@
           scale = 1.5;
         };
       };
+
       prefer-no-csd = true;
-      environment.DISPLAY = ":0";
       hotkey-overlay.skip-at-startup = true;
+
       window-rules = [
         {
           geometry-corner-radius = {
@@ -78,6 +93,7 @@
           clip-to-geometry = true;
         }
       ];
+
       layout = {
         gaps = 8;
         default-column-width = {
@@ -89,6 +105,12 @@
           inactive.color = "#505050";
         };
       };
+
+      environment = {
+        DISPLAY = ":0";
+        LANG = "zh_CN.UTF-8";
+      };
+
       input = {
         touchpad = {
           scroll-factor = 0.25;
--- modules/system.nix
+++ modules/system.nix
@@ -1,6 +1,27 @@
 { config, lib, pkgs, ... }:
 
 {
+  # Core system packages available to all users
+  environment.systemPackages = with pkgs; [
+    git
+    micro
+    fd
+    ripgrep
+  ];
+
+  # Internationalization properties
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocales = [ "zh_CN.UTF-8/UTF-8" ];
+  };
+
+  # Allow installation of unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable services for better file manager functionality
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
+
   # Enable experimental features for flakes
   nix.settings = {
     substituters = [
@@ -10,19 +31,4 @@
 
     experimental-features = [ "nix-command" "flakes" ];
   };
-
-  # Core system packages available to all users
-  environment.systemPackages = with pkgs; [
-    git
-    micro
-    fd
-    ripgrep
-  ];
-
-  # Allow installation of unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable services for better file manager functionality
-  services.gvfs.enable = true;
-  services.tumbler.enable = true;
 }

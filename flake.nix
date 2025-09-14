365b5b57162ea24a5137c1779ef3f99480de40ce
wenxuanjun <wenxuanjun@yahoo.com>
2025-09-14 22:22:30 +0800

    Set ssh.enableDefaultConfig to false

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://nix-community.cachix.org"
+      "https://niri.cachix.org"
     ];
 
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     ];
   };
 
